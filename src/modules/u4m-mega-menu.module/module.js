53afabe3e88cc2a934866206e91674ab5173a1ad
rakeshm-twp <rakeshm@thewebplant.in>
2024-11-21 16:35:21 +0530

    Header Done

---
+++ src/modules/u4m-mega-menu.module/module.js
@@ -0,0 +1,23 @@
+$('.mobileHide .childTrigger').click(function(event){
+	$(this).parents('.menuItem').siblings().removeClass('childOpen');
+	$(this).parents('.menuItem').toggleClass('childOpen');
+	$('html').toggleClass('hdrOpen');
+	event.preventDefault();
+});
+
+
+$('.desktopHide .childTrigger').click(function(){
+	$(this).parent().siblings('.menuItem').removeClass('childOpenMob');
+	$(this).parent('.menuItem').siblings().find('.childMenu').slideUp();
+	$(this).parent('.menuItem').toggleClass('childOpenMob');
+	$(this).next('.childMenu').slideToggle();
+});
+
+
+$('.mobile-trigger').click(function(){
+	$('body').toggleClass('mobile-open');
+	$('.headerMenu').slideToggle();
+	$('.desktopHide .menuItem').removeClass('childOpenMob');
+	$('.desktopHide .childMenu').slideUp();
+	$('html').toggleClass('hdrOpenMob');
+});
