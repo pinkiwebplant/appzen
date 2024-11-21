d5beb2861d853fec1c5698931ccaf75cb1b7cafe
amanHubDev <145353514+amanHubDev@users.noreply.github.com>
2024-11-21 18:32:33 +0530

    Case study llisting loadmore updated

--- src/modules/u4m-case-study-listing.module/module.js
+++ src/modules/u4m-case-study-listing.module/module.js
@@ -4,12 +4,14 @@ $('.filter_sidebar button').click(function() {
 	$(this).parents().siblings().removeClass('active');
 
 	if ($(this).hasClass('active')) {
-	$(this).parents().removeClass('active');
+		$(this).parents().removeClass('active');
 
 		$('.case_listing_wrap').load(window.location.href.split('?')[0] + '?category=-' + value + ' .case_listing_wrap > *');
+		$('.loadmore_btn').load(window.location.href.split('?')[0] + '?category=-' + value + ' .loadmore_btn > *');
 	} else {
-	$(this).parents().addClass('active');
+		$(this).parents().addClass('active');
 
 		$('.case_listing_wrap').load(window.location.href.split('?')[0] + '?category=' + value + ' .case_listing_wrap > *');
+		$('.loadmore_btn').load(window.location.href.split('?')[0] + '?category=' + value + ' .loadmore_btn > *');
 	}
 });
