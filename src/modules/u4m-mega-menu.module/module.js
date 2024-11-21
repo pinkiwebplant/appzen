6e72da6012b52f8d30e0612a9c7cffee42d12cec
rakeshm-twp <rakeshm@thewebplant.in>
2024-11-21 18:00:06 +0530

    Header Done and Information Banner Module Done

--- src/modules/u4m-mega-menu.module/module.js
+++ src/modules/u4m-mega-menu.module/module.js
@@ -1,23 +1,30 @@
-$('.mobileHide .childTrigger').click(function(event){
-	$(this).parents('.menuItem').siblings().removeClass('childOpen');
-	$(this).parents('.menuItem').toggleClass('childOpen');
-	$('html').toggleClass('hdrOpen');
-	event.preventDefault();
+$(".mobileHide .childTrigger").click(function (event) {
+  $(this).parents(".menuItem").siblings().removeClass("childOpen");
+  $(this).parents(".menuItem").toggleClass("childOpen");
+  $("html").toggleClass("hdrOpen");
+  event.preventDefault();
 });
 
-
-$('.desktopHide .childTrigger').click(function(){
-	$(this).parent().siblings('.menuItem').removeClass('childOpenMob');
-	$(this).parent('.menuItem').siblings().find('.childMenu').slideUp();
-	$(this).parent('.menuItem').toggleClass('childOpenMob');
-	$(this).next('.childMenu').slideToggle();
+$("html").click(function () {
+  $(".menuItem").removeClass("childOpen");
+  $("html").removeClass("hdrOpen");
 });
 
+$(".megaMenu").click(function (e) {
+  e.stopPropagation();
+});
 
-$('.mobile-trigger').click(function(){
-	$('body').toggleClass('mobile-open');
-	$('.headerMenu').slideToggle();
-	$('.desktopHide .menuItem').removeClass('childOpenMob');
-	$('.desktopHide .childMenu').slideUp();
-	$('html').toggleClass('hdrOpenMob');
+$(".desktopHide .childTrigger").click(function () {
+  $(this).parent().siblings(".menuItem").removeClass("childOpenMob");
+  $(this).parent(".menuItem").siblings().find(".childMenu").slideUp();
+  $(this).parent(".menuItem").toggleClass("childOpenMob");
+  $(this).next(".childMenu").slideToggle();
+});
+
+$(".mobile-trigger").click(function () {
+  $("body").toggleClass("mobile-open");
+  $(".headerMenu").slideToggle();
+  $(".desktopHide .menuItem").removeClass("childOpenMob");
+  $(".desktopHide .childMenu").slideUp();
+  $("html").toggleClass("hdrOpenMob");
 });
