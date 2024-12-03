7435d9af0a7d5f8ac430d66dc14bf1013c4f4e55
amanHubDev <145353514+amanHubDev@users.noreply.github.com>
2024-12-03 17:12:43 +0530

    Portal Id updates

--- src/modules/u4m-landing-page-classic.module/module.js
+++
@@ -1,6 +0,0 @@
-// Form Listener
-window.addEventListener('message', function (event) {
-  if (event.data.type === 'hsFormCallback' && event.data.eventName === 'onFormSubmit') {
-    $("body").last().addClass("form-submitted");
-  }
-});
